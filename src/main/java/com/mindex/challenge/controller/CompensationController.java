94abdd55e9bf6046ca398093b8fabdb65f55f54b
deemler <39466351+deemler@users.noreply.github.com>
2018-05-20 15:26:10 -0400

    Add files via upload

---
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -0,0 +1,27 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by DEmler on 5/20/2018.
+ */
+@RestController
+public class CompensationController {
+
+	@Autowired
+	private CompensationService compensationService;
+
+	@PostMapping("/compensation")
+	public Compensation create(@RequestBody Compensation compensation) {
+		return compensationService.create(compensation);
+	}
+
+	@GetMapping("/compensation/{id}")
+	public Compensation read(@PathVariable String id) {
+		return compensationService.read(id);
+	}
+
+}
